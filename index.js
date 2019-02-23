68828a331caff9e30217fb09dc5cd65f4bfccd2e
LeFizzy <honterparade@gmail.com>
2019-02-23 20:27:40 +0100

    Major changes & fixes.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const request = require('request');
 
 const appHost = process.env.LOCAL_APP_HOST      || "localhost";
-const appPort = process.env.LOCAL_APP_PORT      || 8080;
+const appPort = process.env.LOCAL_APP_PORT      || 80;
 const wssHost = process.env.CENTRAL_WSS_HOST    || "localhost";
 const wssPort = process.env.CENTRAL_WSS_PORT    || 8081;
 
 const socket = require('socket.io-client')(`http://${wssHost}:${wssPort}`);
 socket.on('connect', () => {
-    console.log('Connected to CentralRouter instance.');
+    console.log(`Connected to CentralRouter instance ${appHost}.`);
 });
 
 socket.on('wss.interfaces.hello', (data) => {
@@ -16,10 +16,11 @@ socket.on('wss.interfaces.hello', (data) => {
 });
 
 socket.on('interfaces.http.get', (httpdata) => {
+    console.log(`[GET] ${httpdata}`); 
+
     request(`http://${appHost}:${appPort}${httpdata}`, (error, response, body) => {
         if(error) throw error;
         
-	    console.log(`[GET] ${httpdata}`); 
         socket.emit('interfaces.http.response', {
             statusCode: response.statusCode,
             bodyData: body
@@ -31,12 +32,14 @@ socket.on('interfaces.http.post', (postdata) => {
     const httpRoute = postdata.route;
     const httpData  = postdata.data;
 
+    console.log(`[POST] ${httpData}`); 
+
     request.post({ 
         url: `http://${appHost}:${appPort}${httpRoute}`, 
         form: {httpData}}, (error, response, body) => {
             if(error) throw error;
 
-            console.log(`[POST] ${httpData}`); 
+            
             socket.emit('interfaces.http.response', {
                 statusCode: response.statusCode,
                 bodyData: body
